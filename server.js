c814a84db91754c683bff051b22c405e0c5cf7bd
Utkarsh Singh <69565691+realutkarshh@users.noreply.github.com>
2026-04-04 10:54:27 +0530

    fix/messages in db config and server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 const start = async () => {
   await connectDB();
   app.listen(PORT, () => {
-    console.log(`🚀 Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
   });
 };
 
